dd9b3684f48c87d0104ddbce3115567298730770
Wind Slightly <70873548+iamSlightlyWind@users.noreply.github.com>
2022-04-05 22:38:07 +0700

    build_kernel.sh: add message for log search

--- build_kernel.sh
+++ build_kernel.sh
@@ -17,3 +17,5 @@ echo "Generate .config from defconfig"
 make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_DEBUG_SECTION_MISMATCH=y vendor/gts7l_eur_open_defconfig
 echo "Build kernel"
 make -j$(nproc) -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE CONFIG_DEBUG_SECTION_MISMATCH=y
+
+echo "Build completed"
